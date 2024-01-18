28e8679a049bb8edbad71b68d251923d378ef011
Dominic Liebel <dominic.liebel@gmail.com>
2024-01-18 13:02:21 +0100

    Added optimized model with 2 dropouts and another linear layer.

--- Anonymization/model.py
+++ Anonymization/model.py
@@ -2,17 +2,23 @@
 
 import torch
 
-class ModifiedModel(torch.nn.Module):
-    """Custom CNN model for image classification."""
-
+class OptimizedModel(torch.nn.Module):
     def __init__(self, input_size, output_size):
-        super().__init__()
-        self.fc1 = torch.nn.Linear(input_size, 256)  # Adjust the size as needed
+        super(OptimizedModel, self).__init__()
+        self.fc1 = torch.nn.Linear(input_size, 512)
         self.relu1 = torch.nn.ReLU()
-        self.fc2 = torch.nn.Linear(256, output_size)
+        self.dropout1 = torch.nn.Dropout(0.5)
+        self.fc2 = torch.nn.Linear(512, 256)
+        self.relu2 = torch.nn.ReLU()
+        self.dropout2 = torch.nn.Dropout(0.5)
+        self.fc3 = torch.nn.Linear(256, output_size)
 
     def forward(self, x):
         x = self.fc1(x)
         x = self.relu1(x)
+        x = self.dropout1(x)
         x = self.fc2(x)
+        x = self.relu2(x)
+        x = self.dropout2(x)
+        x = self.fc3(x)
         return x
