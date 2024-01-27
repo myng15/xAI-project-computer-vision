1ea78171a1708772f110de810b4cb65e77c652f0
myng15 <nguyenhanhhuyenmy@gmail.com>
2024-01-27 00:21:21 +0000

    modify train and test functions for CNNs

--- test.py
+++ test.py
@@ -1,36 +1,39 @@
 import torch
+import torchvision.transforms as transforms
 
 import numpy as np
 import argparse
 
-from datasets import get_datasets, create_data_loaders
+from datasets import get_normalized_cifar_datasets, create_data_loaders
 from utils import get_available_device, DeviceDataLoader, seed_all
-from models import get_model, get_loss_func
+from models import get_model
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', '--num_classes', type=int,
-    help='num_classes = 10 -> the model to be tested is trained on CIFAR-10, if num_classes = 100 -> CIFAR-100')
+                    help='10 -> the model to be tested is trained on CIFAR-10; 100 -> CIFAR-100')
 parser.add_argument('-dt', '--cp_datetime', type=str,
-    help='datetime of the checkpoint to be tested')
+                    help='datetime of the checkpoint to be tested')
+parser.add_argument('-o', '--optim_code', type=str, default='',
+                    help='code of the optimization scheme used when training the model to be tested')
 parser.add_argument('-m', '--model_name', type=str,
-    help='name of model to be tested')
-parser.add_argument('-l', '--loss_func_name', type=str,
-    help='name of loss function for model to be tested')
+                    help='name of model to be tested')
 parser.add_argument('-b', '--batch_size', type=int,
-    help='number of samples per batch to load')
+                    help='number of samples per batch to load')
 parser.add_argument('-w', '--num_workers', type=int,
-    help='number of subprocesses to use for data loading')
+                    help='number of subprocesses to use for data loading')
 args = vars(parser.parse_args())
 
 num_classes = args['num_classes']
 model_name = args['model_name']
 cp_datetime = args['cp_datetime']
-loss_func_name = args['loss_func_name']
+if args['optim_code']:
+    optim_code = args['optim_code']
+else:
+    optim_code = ''
 batch_size = args['batch_size']
 num_workers = args['num_workers']
 
-print(num_classes)
-
+# set seed
 seed_all(42)
 
 # get device
@@ -38,7 +41,7 @@ device = get_available_device()
 print(f"Device: {device}\n")
 
 # get the training, validation and test_datasets
-train_data, test_data = get_datasets(num_classes)
+train_data, test_data = get_normalized_cifar_datasets(num_classes)
 # get the test data loader
 _, _, test_loader = create_data_loaders(
     train_data, test_data, batch_size, num_workers
@@ -55,19 +58,10 @@ elif num_classes == 100:
 else:
     output_folder = None
 
-# load the final model checkpoint
-final_model_cp = torch.load(f'outputs/{output_folder}/final_model_{model_name}_{cp_datetime}.pth')
-final_model_epoch = final_model_cp['epoch']
-print(f"Last model was saved at {final_model_epoch} epochs\n")
-
-# load the best model checkpoint
-best_model_cp = torch.load(f'outputs/{output_folder}/best_model_{model_name}_{cp_datetime}.pth')
-best_model_epoch = best_model_cp['epoch']
-print(f"Best model was saved at {best_model_epoch} epochs\n")
 
 def compute_accuracy(predicted, labels):
     predictions, predicted_labels = torch.max(predicted, dim=1)
-    return torch.tensor(torch.sum(predicted_labels == labels).item()/len(predicted))
+    return torch.tensor(torch.sum(predicted_labels == labels).item() / len(predicted))
 
 
 def evaluate(model, dl, loss_func):
@@ -88,18 +82,20 @@ def evaluate(model, dl, loss_func):
         # Calculate class-wise accuracy
         _, pred = torch.max(predicted, 1)
         correct_tensor = pred.eq(labels.data.view_as(pred))
-        correct = np.squeeze(correct_tensor.numpy()) if not torch.cuda.is_available() else np.squeeze(correct_tensor.cpu().numpy())
+        correct = np.squeeze(correct_tensor.numpy()) if not torch.cuda.is_available() else np.squeeze(
+            correct_tensor.cpu().numpy())
 
         for i in range(len(classes)):
             class_label = classes[i]
-            class_correct[i] += np.sum(correct[labels.data.cpu().numpy() == i]) #np.sum(correct[labels.data == i]) if not torch.cuda.is_available() else
-            class_total[i] += np.sum(labels.data.cpu().numpy() == i) #np.sum(labels.data == i)
+            class_correct[i] += np.sum(correct[labels.data.cpu().numpy() == i])
+            class_total[i] += np.sum(labels.data.cpu().numpy() == i)
 
     test_loss = torch.stack(loss_per_batch).mean().item()
-    #test_accuracy = torch.stack(accuracy_per_batch).mean().item()
+    # test_accuracy = torch.stack(accuracy_per_batch).mean().item()
     test_accuracy = 100. * np.sum(class_correct) / np.sum(class_total)
 
-    print(f'Test Loss: {test_loss:.6f}, Test Accuracy (Overall): {test_accuracy:.2f}% ({np.sum(class_correct)}/{np.sum(class_total)})') #{test_accuracy * 100:.2f}%
+    print(
+        f'Test Loss: {test_loss:.4f}, Test Accuracy (Overall): {test_accuracy:.2f}% ({np.sum(class_correct)}/{np.sum(class_total)})')  # {test_accuracy * 100:.2f}%
 
     for i in range(len(classes)):
         if class_total[i] > 0:
@@ -111,30 +107,43 @@ def evaluate(model, dl, loss_func):
     return test_loss, test_accuracy
 
 
-# test the last epoch saved model
 def test_final_model(model, checkpoint, test_loader):
+    """
+    Test the last epoch saved model
+    """
     print('Loading last epoch saved model weights...')
     model.load_state_dict(checkpoint['model_state_dict'])
-    loss_func = get_loss_func(loss_func_name)
+    loss_func = checkpoint['loss_func']
 
     with torch.set_grad_enabled(False):
         evaluate(model, test_loader, loss_func)
-    #print(f"Last epoch saved model accuracy: {test_acc:.3f}")
 
-# test the best epoch saved model
+
 def test_best_model(model, checkpoint, test_loader):
-    print('-' * 50)
+    """
+    Test the best epoch saved model
+    """
     print('Loading best epoch saved model weights...')
     model.load_state_dict(checkpoint['model_state_dict'])
-    loss_func = get_loss_func(loss_func_name)
+    loss_func = checkpoint['loss_func']
 
     with torch.set_grad_enabled(False):
         evaluate(model, test_loader, loss_func)
-    #print(f"Best epoch saved model accuracy: {test_acc:.3f}")
-
 
 
 if __name__ == '__main__':
     model = get_model(model_name, batch_size, num_classes)
+
+    # load and test the final model checkpoint
+    final_model_cp = torch.load(f'outputs/{output_folder}/final_model_{model_name}{optim_code}_{cp_datetime}.pth')
+    final_model_epoch = final_model_cp['epoch']
+    print(f"Final model was saved at {final_model_epoch} epochs\n")
     test_final_model(model, final_model_cp, test_dl)
+
+    print('-' * 50)
+
+    # load and test the best model checkpoint
+    best_model_cp = torch.load(f'outputs/{output_folder}/best_model_{model_name}{optim_code}_{cp_datetime}.pth')
+    best_model_epoch = best_model_cp['epoch']
+    print(f"Best model was saved at {best_model_epoch} epochs\n")
     test_best_model(model, best_model_cp, test_dl)
--- train.py
+++ train.py
@@ -1,34 +1,11 @@
 import torch
+import torch.nn as nn
 
 import datetime
 
-from datasets import get_datasets, create_data_loaders
-from utils import seed_all, save_final_model, save_plots, BestModelSaver, get_available_device, DeviceDataLoader
-
-# get device
-device = get_available_device()
-print(f"Device: {device}\n")
-
-# construct the argument parser
-# parser = argparse.ArgumentParser()
-# parser.add_argument('-b', '--batch_size', type=int, default=64,
-#     help='number of samples per batch to load')
-# parser.add_argument('-e', '--epochs', type=int, default=10,
-#     help='number of epochs to train the model for')
-# parser.add_argument('-lr', type=int, default=0.001,
-#     help='learning rate')
-# args = vars(parser.parse_args())
-#
-# batch_size = args['batch_size']
-# epochs = args['epochs']
-# lr = args['lr']
-
-
-# LATER: Decay LR by a factor of 0.1 every 7 epochs
-#exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
-
-# initialize SaveBestModel class
-save_best_model = BestModelSaver()
+from datasets import prepare_data_loaders
+from utils import seed_all, save_final_model, BestModelSaver, get_available_device
+from visualization import save_plots
 
 
 def compute_accuracy(predicted, labels):
@@ -50,30 +27,26 @@ def validate(model, dl, loss_func):
     return epoch_val_loss, epoch_val_acc
 
 
-def prepare_data_loaders(num_classes, batch_size, num_workers):
-    # get the training, validation and test_datasets
-    train_data, test_data = get_datasets(num_classes)
-    # get the training and validation data loaders
-    train_loader, valid_loader, _ = create_data_loaders(
-        train_data, test_data, batch_size, num_workers
-    )
-
-    # Move all the tensors to GPU if available
-    train_dl = DeviceDataLoader(train_loader, device)
-    valid_dl = DeviceDataLoader(valid_loader, device)
-
-    return train_dl, valid_dl
-
-
-def train(num_classes, model, model_name, batch_size, num_workers, epochs, max_lr, loss_func, optimizer): # LATER: add parameter lr_scheduler
-    # seed needed due to batch shuffling?
+def train(num_classes, model, model_name,
+          batch_size, num_workers, epochs,
+          lr_scheduler,
+          loss_func, optimizer, grad_clip,
+          train_transform, test_transform,
+          patience=7, gamma=0.5, #or gamma=0.1,
+          optim_code=''
+          ):
     seed_all(42)
 
-    train_dl, valid_dl = prepare_data_loaders(num_classes, batch_size, num_workers)
+    train_dl, valid_dl = prepare_data_loaders(num_classes, batch_size, num_workers, train_transform, test_transform)
+
+    plot_lrs = False
 
     # start training
     results = []
     current_datetime = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+
+    # initialize SaveBestModel class
+    save_best_model = BestModelSaver()
 
     for epoch in range(epochs):
         model.train()
@@ -87,20 +60,27 @@ def train(num_classes, model, model_name, batch_size, num_workers, epochs, max_l
             train_losses.append(loss)
             optimizer.zero_grad()
             loss.backward()
+
+            # OPTIMIZATION: Gradient clipping
+            if grad_clip:
+                nn.utils.clip_grad_value_(model.parameters(), grad_clip)
+
             optimizer.step()
 
-            # keep track of learning rate
+            # OPTIMIZATION: Keep track of learning rate
             lrs.append(optimizer.param_groups[0]['lr'])
+
             train_accuracies.append(compute_accuracy(predicted, labels))
 
             # logging
             if not batch_idx % 120:
                 print (f'Epoch: {epoch+1:03d}/{epochs:03d} | '
-                      f'Batch {batch_idx:03d}/{len(train_dl):03d} | '
+                      f'Batch {batch_idx+1:03d}/{len(train_dl):03d} | '
                       f'Loss: {loss:.4f}')
 
-        # LATER:
-        #lr_scheduler.step()
+        if lr_scheduler:
+            lr_scheduler.step()
+            plot_lrs = True
 
         # no need to build the computation graph for backprop when computing accuracy
         with torch.set_grad_enabled(False):
@@ -112,24 +92,23 @@ def train(num_classes, model, model_name, batch_size, num_workers, epochs, max_l
                         'avg_val_acc': epoch_val_acc,
                         'avg_train_loss': epoch_train_loss,
                         'avg_train_acc': epoch_train_acc,
-                        'lrs' : lrs})
+                        'lrs': lrs})
 
         # print training/validation statistics
-        print(f'Epoch: {epoch+1:03d}/{epochs:03d}   Train Loss: {epoch_train_loss:.2f} | Train Acc.: {epoch_train_acc:.2f}%'
-              f' | Validation Loss: {epoch_val_loss:.2f} | Validation Acc.: {epoch_val_acc:.2f}%')
+        print(f'Epoch: {epoch+1:03d}/{epochs:03d}   Train Loss: {epoch_train_loss:.4f} | Train Acc.: {epoch_train_acc*100:.2f}%'
+              f' | Validation Loss: {epoch_val_loss:.4f} | Validation Acc.: {epoch_val_acc*100:.2f}%')
 
         # save model if validation loss has decreased (validation loss ever increasing indicates possible overfitting.)
         save_best_model(
-            num_classes, current_datetime, model, model_name, optimizer, loss_func, epoch_val_loss, epoch
+            num_classes, current_datetime, model, model_name, optimizer, loss_func, epoch_val_loss, epoch, optim_code
         )
         print('-' * 50)
 
     # save the trained model weights for a final time
-    save_final_model(num_classes, current_datetime, model, model_name, optimizer, loss_func, epochs)
+    save_final_model(num_classes, current_datetime, model, model_name, optimizer, loss_func, epochs, results, optim_code)
     # save the loss and accuracy plots
-    save_plots(num_classes, current_datetime, model_name, epochs, results)
+    save_plots(num_classes, current_datetime, model_name, epochs, results, optim_code, plot_lrs)
     print('TRAINING FINISHED')
 
-    return results
 
 
